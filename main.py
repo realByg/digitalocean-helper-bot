ee0d8ef42d6a1ac7a5677f49b5cb559e1ac06c83
ZayaByghead <ZayaByghead@gmail.com>
2021-08-14 21:36:54 +0800

    bruh

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
 def parse_config():
-    try:
-        import json
-        from os import environ
+    import json
+    from os import environ
 
-        config = json.load(open('config.json', 'r', encoding='utf-8'))
-        environ['bot_name'] = config['BOT']['NAME']
-        environ['bot_token'] = config['BOT']['TOKEN']
-        environ['bot_admins'] = json.dumps(config['BOT']['ADMINS'])
-
-    except Exception:
-        raise Exception('ERROR: Invalid config')
+    config = json.load(open('config.json', 'r', encoding='utf-8'))
+    environ['bot_name'] = config['BOT']['NAME']
+    environ['bot_token'] = config['BOT']['TOKEN']
+    environ['bot_admins'] = json.dumps(config['BOT']['ADMINS'])
 
 
 def start_bot():
-    try:
-        from bot import bot
+    from bot import bot
 
-        bot.polling(none_stop=True)
-
-    except Exception:
-        raise Exception('ERROR: Failed to start bot')
+    bot.polling(none_stop=True)
 
 
 if __name__ == '__main__':
